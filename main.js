a81a7ee441be2a65e6757c40cad8b567ef6bb29d
Anderson Araujo <anderson.dearaujo.sousa@gmail.com>
2024-05-10 03:21:54 -0300

    sessão Faq

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
     document.querySelector('nav').classList.toggle('window-scroll-home', window.scrollY < 100)
 })
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
+
+        const icone = faq.querySelector('.icone__faq i');
+        if(icone.className === 'uil uil-plus'){
+            icone.className = "uil uil-minus";
+        }
+        else{
+            icone.className = "uil uil-plus";
+        }
+    })
+}) 
